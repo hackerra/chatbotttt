67c17cec8000555de375cc005cd7642ac447d3ca
hackerra <60672741+hackerra@users.noreply.github.com>
2020-04-27 11:16:07 -0700

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -8,7 +8,7 @@ english_bot.set_trainer(ChatterBotCorpusTrainer)
 english_bot.train("chatterbot.corpus.english")
 
 #Setting telegram things
-tg_token='YOUR_BOT_TOKEN_HERE' # Refer README for more details
+tg_token='1163071540:AAERAqEitjVDmuVP8b4gYRw8BUARc7LbTHs'
 import logging
 from telegram.ext import CommandHandler,MessageHandler, Filters,Updater
 updater = Updater(token=tg_token)
